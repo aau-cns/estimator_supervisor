6d1f0b1fa3e2f530c9a09e769dd8e6a1e987c815
Fornasier, Alessandro <alessandro.fornasier@aau.at>
2022-06-24 09:20:29 +0200

    [FIX] Fixed undefined behavior, and changed format.
    Fixed wrong access to buffer end(), changed to back() [end()-1]

--- src/estimator_supervisor.cpp
+++ src/estimator_supervisor.cpp
@@ -1,8 +1,7 @@
-// Copyright (C) 2021 Christian Brommer and Alessandro Fornasier,
+// Copyright (C) 2021 Alessandro Fornasier,
 // Control of Networked Systems, Universitaet Klagenfurt, Austria
 //
-// You can contact the author at <christian.brommer@ieee.org>
-// and <alessandro.fornasier@ieee.org>
+// You can contact the author at <alessandro.fornasier@ieee.org>
 //
 // All rights reserved.
 //
@@ -17,15 +16,18 @@
 #include "estimator_supervisor.h"
 #include "utils/colors.h"
 
-Supervisor::Supervisor(ros::NodeHandle &nh, double &window, double &max_norm, std::string &topic, std::string &msg_type) :
-  nh_(nh), window_(window), max_norm_(max_norm), topic_(topic), msg_type_(msg_type){
+Supervisor::Supervisor(ros::NodeHandle &nh, double &window, double &max_norm,
+                       std::string &topic, std::string &msg_type)
+    : nh_(nh), window_(window), max_norm_(max_norm), topic_(topic),
+      msg_type_(msg_type) {
 
   // Advertise supervisor service
-  srv_ = nh_.advertiseService("service/supervision", &Supervisor::serviceHandler, this);
-
+  srv_ = nh_.advertiseService("service/supervision",
+                              &Supervisor::serviceHandler, this);
 }
 
-bool Supervisor::serviceHandler(std_srvs::Trigger::Request& , std_srvs::Trigger::Response& res) {
+bool Supervisor::serviceHandler(std_srvs::Trigger::Request &,
+                                std_srvs::Trigger::Response &res) {
 
   std::cout << YELLOW("Supervisor service called") << std::endl;
 
@@ -35,14 +37,17 @@ bool Supervisor::serviceHandler(std_srvs::Trigger::Request& , std_srvs::Trigger:
 
   // Subscribe to estimator and buffer data
   if (msg_type_ == "posestamped") {
-    sub_estimate_ = nh_.subscribe(topic_, 1, &Supervisor::estimateCallback, this);
+    sub_estimate_ =
+        nh_.subscribe(topic_, 1, &Supervisor::estimateCallback, this);
   } else if (msg_type_ == "posewithcovariancestamped") {
-    sub_estimate_ = nh_.subscribe(topic_, 1, &Supervisor::estimateWithCovarianceCallback, this);
+    sub_estimate_ = nh_.subscribe(
+        topic_, 1, &Supervisor::estimateWithCovarianceCallback, this);
   }
 
   // Wait till the window gets filled by measurements
   while (!buffer_full_) {
-    std::cout << YELLOW("Waiting for filling the estimate buffer...") << std::endl;
+    std::cout << YELLOW("Waiting for filling the estimate buffer...")
+              << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
@@ -58,29 +63,41 @@ bool Supervisor::serviceHandler(std_srvs::Trigger::Request& , std_srvs::Trigger:
 
 bool Supervisor::estimatorSupervision() {
 
-  // Compare the norm of  difference between the
+  // Safety check
+  if (position_buffer_.front().timestamp <= 0.0 ||
+      position_buffer_.back().timestamp <= 0.0) {
+    std::cout << RED("Buffer corrupted: first or last measurement has invalid "
+                     "timestamp")
+              << std::endl;
+    return false;
+  }
+
+  // Compare the norm of difference between the
   // first and last element of the buffer
-  if ((position_buffer_.end()->p - position_buffer_.begin()->p).norm() > max_norm_) {
+  if ((position_buffer_.back().p - position_buffer_.front().p).norm() >
+      max_norm_) {
     return false;
   }
 
   return true;
 }
 
-void Supervisor::estimateWithCovarianceCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
+void Supervisor::estimateWithCovarianceCallback(
+    const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg) {
 
   // Define local data structure
   Buffer::positionBuffer data;
 
   // Parse message to defined data structure
   data.timestamp = msg->header.stamp.toSec();
-  data.p << msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z;
+  data.p << msg->pose.pose.position.x, msg->pose.pose.position.y,
+      msg->pose.pose.position.z;
 
   // Push data into buffer
   position_buffer_.emplace_back(data);
 
   // Remove oldest if sensor reading window width is reached
-  if ((data.timestamp - position_buffer_.begin()->timestamp) > window_) {
+  if ((data.timestamp - position_buffer_.front().timestamp) > window_) {
 
     // Set reached window flag
     buffer_full_ = true;
@@ -91,7 +108,8 @@ void Supervisor::estimateWithCovarianceCallback(const geometry_msgs::PoseWithCov
   }
 }
 
-void Supervisor::estimateCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+void Supervisor::estimateCallback(
+    const geometry_msgs::PoseStamped::ConstPtr &msg) {
 
   // Define local data structure
   Buffer::positionBuffer data;
@@ -104,7 +122,7 @@ void Supervisor::estimateCallback(const geometry_msgs::PoseStamped::ConstPtr& ms
   position_buffer_.emplace_back(data);
 
   // Remove oldest if sensor reading window width is reached
-  if ((data.timestamp - position_buffer_.begin()->timestamp) > window_) {
+  if ((data.timestamp - position_buffer_.front().timestamp) > window_) {
 
     // Set reached window flag
     buffer_full_ = true;
